326a005c6779ae7c2cd782cf183ec43a42c33827
Rael Garcia <raelga@gmail.com>
2017-06-10 20:55:23 +0200

    Fix linter issues.

--- dist/tsinvaders.js
+++ dist/tsinvaders.js
@@ -1,18 +1,18 @@
-/// <reference path="lib/phaser.d.ts" />
+/// <reference path="../node_modules/phaser/typescript/phaser.d.ts" />
 var state;
 var world;
-var game = new Phaser.Game(1600, 1024, Phaser.AUTO, 'tsinvaders', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(1600, 1024, Phaser.AUTO, "tsinvaders", { preload: preload, create: create, update: update, render: null });
 function preload() {
     var assetsPath = "assets/base/png/";
     var images = [
-        { name: 'playerBullet', file: 'playerBullet.png' },
-        { name: 'enemyBullet', file: 'enemyBullet.png' },
-        { name: 'player', file: 'player.png' },
-        { name: 'background', file: 'background.png' }
+        { name: "playerBullet", file: "playerBullet.png" },
+        { name: "enemyBullet", file: "enemyBullet.png" },
+        { name: "player", file: "player.png" },
+        { name: "background", file: "background.png" }
     ];
     images.forEach(function (i) { return game.load.image(i.name, assetsPath + i.file); });
     var sprites = [
-        { name: 'enemy', file: 'enemy.png', w: 200, h: 200 },
+        { name: "enemy", file: "enemy.png", w: 200, h: 200 },
     ];
     sprites.forEach(function (s) { return game.load.spritesheet(s.name, assetsPath + s.file, s.w, s.h); });
 }
@@ -24,8 +24,6 @@ function update() {
     state.update();
     world.update();
 }
-function render() {
-}
 var Player = (function () {
     function Player(shipImage, bulletImage) {
         var _this = this;
@@ -33,54 +31,55 @@ var Player = (function () {
         this.PLAYER_SPEED = 300;
         this.BULLET_SPEED = -600;
         this.FIRE_COOLDOWN = 10;
-        // Create the player ship
+        // create the player ship
         this.ship = game.add.sprite(game.stage.width / 2, game.stage.height - 100, shipImage),
             game.physics.enable(this.ship, Phaser.Physics.ARCADE);
         this.ship.width = 50;
         this.ship.height = 75;
-        // Create the bullet group for the player
+        // create the bullet group for the players
         this.bullets = game.add.group();
         this.bullets.enableBody = true;
         this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
-        // Create the bullets pool
+        // create the bullets pool
         this.bullets.createMultiple(30, bulletImage);
         var settings = [
-            { name: 'width', value: 20 },
-            { name: 'height', value: 40 },
-            { name: 'anchor.x', value: -0.75 },
-            { name: 'anchor.y', value: 1 },
-            { name: 'outOfBoundsKill', value: true },
-            { name: 'checkWorldBounds', value: true },
+            { name: "width", value: 20 },
+            { name: "height", value: 40 },
+            { name: "anchor.x", value: -0.75 },
+            { name: "anchor.y", value: 1 },
+            { name: "outOfBoundsKill", value: true },
+            { name: "checkWorldBounds", value: true },
         ];
         settings.forEach(function (s) { return _this.bullets.setAll(s.name, s.value); });
     }
     Player.prototype.update = function () {
-        // Reduce the fire cooldown
-        if (this.cooldown > 0)
+        // reduce the fire cooldown
+        if (this.cooldown > 0) {
             this.cooldown--;
-        // Reset the velocity to stop the ship
+        }
+        // reset the velocity to stop the ship
         this.ship.body.velocity.setTo(0, 0);
     };
     Player.prototype.move = function (x, y, speed) {
         if (x === void 0) { x = 0; }
         if (y === void 0) { y = 0; }
         if (speed === void 0) { speed = this.PLAYER_SPEED; }
-        // Move the ship
+        // move the ship
         this.ship.body.velocity.setTo(x * speed, y * speed);
     };
     Player.prototype.fire = function (speed) {
         if (speed === void 0) { speed = this.BULLET_SPEED; }
-        // Check cooldown
+        // check cooldown
         if (!this.cooldown) {
-            // Get a bullet from the bullets pool
+            // get a bullet from the bullets pool
             var bullet = this.bullets.getFirstExists(false);
-            // If a bullet is available
+            // ff a bullet is available
             if (bullet) {
-                // Bullet starting position above the player ship
+                // bullet starting position above the player ship
                 bullet.reset(world.player.ship.x, world.player.ship.y + 8);
-                // Bullet fired!
+                // bullet fired!
                 bullet.body.velocity.y = this.BULLET_SPEED;
-                // Set up the cooldown
+                // set up the cooldown
                 this.cooldown = this.FIRE_COOLDOWN;
             }
         }
@@ -89,96 +88,97 @@ var Player = (function () {
 }());
 var Enemies = (function () {
     function Enemies(shipImage, bulletImage) {
-        if (shipImage === void 0) { shipImage = 'enemy'; }
-        if (bulletImage === void 0) { bulletImage = 'enemyBullet'; }
+        if (shipImage === void 0) { shipImage = "enemy"; }
+        if (bulletImage === void 0) { bulletImage = "enemyBullet"; }
         var _this = this;
         this.cooldown = 0;
         this.BULLET_SPEED = 100;
         this.FIRE_COOLDOWN = 10;
-        // Create the ships group for the enemies
+        // create the ships group for the enemies
         this.ships = game.add.group();
         this.ships.enableBody = true;
         this.ships.physicsBodyType = Phaser.Physics.ARCADE;
-        // Create the enemy fleet
+        // create the enemy fleet
         this.createEnemyFleet();
-        // Create the bullet group for the enemies
+        // create the bullet group for the enemies
         this.bullets = game.add.group();
         this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
         this.bullets.enableBody = true;
-        // Create the bullets pool
+        // create the bullets pool
         this.bullets.createMultiple(30, bulletImage);
         var settings = [
-            { name: 'width', value: 20 },
-            { name: 'height', value: 20 },
-            { name: 'anchor.x', value: 0.5 },
-            { name: 'anchor.y', value: 1 },
-            { name: 'outOfBoundsKill', value: true },
-            { name: 'checkWorldBounds', value: true },
+            { name: "width", value: 20 },
+            { name: "height", value: 20 },
+            { name: "anchor.x", value: 0.5 },
+            { name: "anchor.y", value: 1 },
+            { name: "outOfBoundsKill", value: true },
+            { name: "checkWorldBounds", value: true },
         ];
         settings.forEach(function (s) { return _this.bullets.setAll(s.name, s.value); });
     }
     Enemies.prototype.createEnemyFleet = function (image, rows, columns, difficulty) {
         var _this = this;
-        if (image === void 0) { image = 'enemy'; }
+        if (image === void 0) { image = "enemy"; }
         if (rows === void 0) { rows = 4; }
         if (columns === void 0) { columns = 7; }
         if (difficulty === void 0) { difficulty = 1; }
-        // Set the enemy Ship box Size
+        // set the enemy Ship box Size
         var box = {
             width: 56,
             height: 56,
             spacing: 8,
         };
-        // Populate the ememy fleet
+        // populate the ememy fleet
         for (var y = 0; y < rows; y++) {
             var xshift = (y % 2) ? 20 : 0;
             for (var x = 0; x < columns; x++) {
                 this.setupEnemyShip(xshift + x * (box.width + box.spacing), y * (box.height + box.spacing), box.width, box.height, image);
             }
         }
-        // Group staring position
+        // group staring position
         this.ships.x = 100;
         this.ships.y = 50;
-        // Move the group edge to edge and loop
+        // move the group edge to edge and loop
         var tween = game.add.tween(this.ships).to({ x: game.width - box.width * columns }, 6000 / (difficulty ? difficulty : 1), Phaser.Easing.Linear.None, true, 0, 1000, true);
-        // Descend on loop
+        // descend on loop
         tween.onRepeat.add(function () { return _this.ships.y += game.height / 10; }, this);
     };
     Enemies.prototype.setupEnemyShip = function (x, y, width, height, image) {
-        // Create the enemy ship
+        // create the enemy ship
         var ship = this.ships.create(x, y, image);
         ship.width = width;
         ship.height = height;
         ship.anchor.setTo(0.5, 0.5);
-        // Animate the ships
-        ship.animations.add('animate', null, 10, true);
-        ship.play('animate');
+        // animate the ships
+        ship.animations.add("animate", null, 10, true);
+        ship.play("animate");
         ship.body.moves = false;
     };
     Enemies.prototype.update = function () {
-        this.bullets.forEach(function (b) { return b.angle++; });
-        // Reduce the fire cooldown
-        if (this.cooldown > 0)
+        // this.bullets.forEach((b: Phaser.Sprite) => b.angle++);
+        // reduce the fire cooldown
+        if (this.cooldown > 0) {
             this.cooldown--;
+        }
         if ((game.rnd.integerInRange(1, 1000) / state.level) < 10) {
             this.fire();
         }
     };
     Enemies.prototype.fire = function (speed) {
         if (speed === void 0) { speed = this.BULLET_SPEED; }
-        // Check cooldown
+        // check cooldown
         if (!this.cooldown) {
-            // Get a bullet from the bullets pool
+            // get a bullet from the bullets pool
             var bullet = this.bullets.getFirstExists(false);
-            // If a bullet is available and at least one enemy alive
+            // if a bullet is available and at least one enemy alive
             if (bullet && this.ships.getFirstAlive()) {
-                // Randomly select one of them
+                // randomly select one of them
                 var shooter = this.ships.getRandom();
-                // Bullet starting position below the shooter ship
+                // bullet starting position below the shooter ship
                 bullet.reset(shooter.body.x, shooter.body.y);
-                // And fire the bullet from this enemy to the player
+                // fire the bullet from this enemy to the player
                 game.physics.arcade.moveToObject(bullet, world.player.ship, this.BULLET_SPEED * state.level);
-                // Set up the cooldown
+                // set up the cooldown
                 this.cooldown = this.FIRE_COOLDOWN;
             }
         }
@@ -191,31 +191,28 @@ var State = (function () {
         this.setupInput();
     }
     State.prototype.setupInput = function () {
-        // Set keyboard arrows as input
+        // set keyboard arrows as input
         this.cursors = game.input.keyboard.createCursorKeys();
-        // Set spacebar as fire key
+        // set spacebar as fire key
         this.fireKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     };
     State.prototype.update = function () {
-        // Update the player
+        // update the player
         world.player.update();
-        // Update the enemies
+        // update the enemies
         world.enemies.update();
-        // Move the player if right or left arrows are pressed
-        if (this.cursors.right.isDown || this.cursors.left.isDown) {
-            world.player.move(~~this.cursors.right.isDown - ~~this.cursors.left.isDown, 0);
-        }
-        // Fire if up arrow or the fire key are pressed
+        // move the player if right or left arrows are pressed
+        world.player.move((this.cursors.right.isDown ? 1 : 0) - (this.cursors.left.isDown ? 1 : 0), 0);
+        // fire if up arrow or the fire key are pressed
         if (this.cursors.up.isDown || this.fireKey.isDown) {
             world.player.fire();
         }
-        ;
     };
     return State;
 }());
 var World = (function () {
     function World() {
-        // Initialize the game engine and elements
+        // initialize the game engine and elements
         game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
         game.physics.startSystem(Phaser.Physics.ARCADE);
         this.setuptBackground();
@@ -223,30 +220,28 @@ var World = (function () {
         this.setupEnemies();
     }
     World.prototype.setuptBackground = function (image) {
-        if (image === void 0) { image = 'background'; }
+        if (image === void 0) { image = "background"; }
         this.background = game.add.tileSprite(0, 0, game.stage.width, game.stage.height, image);
     };
     World.prototype.setupPlayer = function (shipImage, bulletImage) {
-        if (shipImage === void 0) { shipImage = 'player'; }
-        if (bulletImage === void 0) { bulletImage = 'playerBullet'; }
+        if (shipImage === void 0) { shipImage = "player"; }
+        if (bulletImage === void 0) { bulletImage = "playerBullet"; }
         this.player = new Player(shipImage, bulletImage);
     };
     World.prototype.setupEnemies = function (shipImage, bulletImage) {
-        if (shipImage === void 0) { shipImage = 'enemy'; }
-        if (bulletImage === void 0) { bulletImage = 'enemyBullet'; }
+        if (shipImage === void 0) { shipImage = "enemy"; }
+        if (bulletImage === void 0) { bulletImage = "enemyBullet"; }
         this.enemies = new Enemies(shipImage, bulletImage);
     };
     World.prototype.update = function () {
-        // Scroll the background to simulate movement
+        // scroll the background to simulate movement
         this.scrollBackground();
     };
     World.prototype.scrollBackground = function (x, y) {
         if (x === void 0) { x = 0; }
         if (y === void 0) { y = 3; }
-        if (x)
-            this.background.tilePosition.x += x;
-        if (y)
-            this.background.tilePosition.y += y;
+        this.background.tilePosition.x += x;
+        this.background.tilePosition.y += y;
     };
     return World;
 }());
--- src/tsinvaders.ts
+++ src/tsinvaders.ts
@@ -1,52 +1,48 @@
-/// <reference path="lib/phaser.d.ts" />
+/// <reference path="../node_modules/phaser/typescript/phaser.d.ts" />
 
 var state: State;
 var world: World;
 
-var game = new Phaser.Game(
-  1600, 1024, Phaser.AUTO, 'tsinvaders',
-  { preload: preload, create: create, update: update, render: render }
+var game: Phaser.Game = new Phaser.Game(
+  1600, 1024, Phaser.AUTO, "tsinvaders",
+  { preload: preload, create: create, update: update, render: null }
 );
 
-function preload() {
+function preload(): void  {
 
-  var assetsPath: String = "assets/base/png/";
+  const assetsPath: String = "assets/base/png/";
 
-  var images = [
-    { name: 'playerBullet', file: 'playerBullet.png' },
-    { name: 'enemyBullet', file: 'enemyBullet.png' },
-    { name: 'player', file: 'player.png' },
-    { name: 'background', file: 'background.png' }
+  let images: any = [
+    { name: "playerBullet", file: "playerBullet.png" },
+    { name: "enemyBullet", file: "enemyBullet.png" },
+    { name: "player", file: "player.png" },
+    { name: "background", file: "background.png" }
   ]; images.forEach(i => game.load.image(i.name, assetsPath + i.file));
 
-  var sprites = [
-    { name: 'enemy', file: 'enemy.png', w: 200, h: 200 },
+  let sprites: any = [
+    { name: "enemy", file: "enemy.png", w: 200, h: 200 },
   ]; sprites.forEach(s => game.load.spritesheet(s.name, assetsPath + s.file, s.w, s.h));
 
 }
 
-function create() {
+function create(): void {
 
   world = new World();
   state = new State();
 
 }
 
-function update() {
+function update(): void {
 
   state.update();
   world.update();
 
 }
 
-function render() {
-
-}
-
 class Player {
 
-  public ship:     Phaser.Sprite;
-  public bullets:  Phaser.Group;
+  public ship: Phaser.Sprite;
+  public bullets: Phaser.Group;
   public lives?: Phaser.Group;
 
   private cooldown: number = 0;
@@ -57,68 +53,73 @@ class Player {
 
   constructor(shipImage: string, bulletImage: string) {
 
-    // Create the player ship
+    // create the player ship
     this.ship = game.add.sprite(game.stage.width / 2, game.stage.height - 100, shipImage),
     game.physics.enable(this.ship, Phaser.Physics.ARCADE);
     this.ship.width = 50;
     this.ship.height = 75;
 
-    // Create the bullet group for the player
-    this.bullets = game.add.group()
+    // create the bullet group for the players
+    this.bullets = game.add.group();
     this.bullets.enableBody = true;
     this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
 
-    // Create the bullets pool
-    this.bullets.createMultiple(30, bulletImage); 
-    var settings = [
-      { name: 'width', value: 20 },
-      { name: 'height', value: 40 },
-      { name: 'anchor.x', value: -0.75 },
-      { name: 'anchor.y', value: 1 },
-      { name: 'outOfBoundsKill', value: true },
-      { name: 'checkWorldBounds', value: true },
+    // create the bullets pool
+    this.bullets.createMultiple(30, bulletImage);
+    var settings: any = [
+      { name: "width", value: 20 },
+      { name: "height", value: 40 },
+      { name: "anchor.x", value: -0.75 },
+      { name: "anchor.y", value: 1 },
+      { name: "outOfBoundsKill", value: true },
+      { name: "checkWorldBounds", value: true },
     ]; settings.forEach(s => this.bullets.setAll(s.name, s.value));
 
   }
 
-  public update() {
+  public update(): void  {
 
-    // Reduce the fire cooldown
-    if (this.cooldown > 0) this.cooldown--;
+    // reduce the fire cooldown
+    if (this.cooldown > 0) {
+      this.cooldown--;
+    }
 
-    // Reset the velocity to stop the ship
+    // reset the velocity to stop the ship
     this.ship.body.velocity.setTo(0, 0);
 
   }
 
-  public move(x: number = 0, y: number = 0, speed: number = this.PLAYER_SPEED) {
+  public move(x: number = 0, y: number = 0, speed: number = this.PLAYER_SPEED): void  {
 
-    // Move the ship
+    // move the ship
     this.ship.body.velocity.setTo(x * speed, y * speed);
 
   }
 
-  public fire(speed: number = this.BULLET_SPEED) {
+  public fire(speed: number = this.BULLET_SPEED): void  {
 
-    // Check cooldown
+    // check cooldown
     if (!this.cooldown) {
 
-      // Get a bullet from the bullets pool
-      var bullet = this.bullets.getFirstExists(false);
+      // get a bullet from the bullets pool
+      let bullet: Phaser.Sprite = this.bullets.getFirstExists(false);
 
-      // If a bullet is available
+      // ff a bullet is available
       if (bullet) {
 
-        // Bullet starting position above the player ship
+        // bullet starting position above the player ship
         bullet.reset(world.player.ship.x, world.player.ship.y + 8);
 
-        // Bullet fired!
+        // bullet fired!
         bullet.body.velocity.y = this.BULLET_SPEED;
 
-        // Set up the cooldown
+        // set up the cooldown
         this.cooldown = this.FIRE_COOLDOWN;
+
       }
+
     }
+
   }
 
 }
@@ -129,51 +130,51 @@ class Enemies {
   bullets:  Phaser.Group;
 
   private cooldown: number = 0;
-  
+
   private BULLET_SPEED: number = 100;
   private FIRE_COOLDOWN: number = 10;
 
-  constructor(shipImage: string = 'enemy', bulletImage: string = 'enemyBullet') {
+  constructor(shipImage: string = "enemy", bulletImage: string = "enemyBullet") {
 
-    // Create the ships group for the enemies
-    this.ships = game.add.group()
+    // create the ships group for the enemies
+    this.ships = game.add.group();
     this.ships.enableBody = true;
     this.ships.physicsBodyType = Phaser.Physics.ARCADE;
 
-    // Create the enemy fleet
+    // create the enemy fleet
     this.createEnemyFleet();
 
-    // Create the bullet group for the enemies
-    this.bullets = game.add.group()
+    // create the bullet group for the enemies
+    this.bullets = game.add.group();
     this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
     this.bullets.enableBody = true;
 
-    // Create the bullets pool
+    // create the bullets pool
     this.bullets.createMultiple(30, bulletImage);
-    var settings = [
-      { name: 'width', value: 20 },
-      { name: 'height', value: 20 },
-      { name: 'anchor.x', value: 0.5 },
-      { name: 'anchor.y', value: 1 },
-      { name: 'outOfBoundsKill', value: true },
-      { name: 'checkWorldBounds', value: true },
+    let settings: any = [
+      { name: "width", value: 20 },
+      { name: "height", value: 20 },
+      { name: "anchor.x", value: 0.5 },
+      { name: "anchor.y", value: 1 },
+      { name: "outOfBoundsKill", value: true },
+      { name: "checkWorldBounds", value: true },
     ]; settings.forEach(s => this.bullets.setAll(s.name, s.value));
 
   }
 
-  public createEnemyFleet(image: string = 'enemy', rows: number = 4, columns: number = 7, difficulty: number = 1) {
+  public createEnemyFleet(image: string = "enemy", rows: number = 4, columns: number = 7, difficulty: number = 1): void  {
 
-    // Set the enemy Ship box Size
-    let box = {
+    // set the enemy Ship box Size
+    const box: any = {
       width: 56,
       height: 56,
       spacing: 8,
-    }
+    };
 
-    // Populate the ememy fleet
-    for (var y = 0; y < rows; y++) {
-      const xshift = (y % 2) ? 20 : 0;
-      for (var x = 0; x < columns; x++) {
+    // populate the ememy fleet
+    for (var y: number = 0; y < rows; y++) {
+      const xshift: number = (y % 2) ? 20 : 0;
+      for (var x: number = 0; x < columns; x++) {
         this.setupEnemyShip(
           xshift + x * (box.width + box.spacing),
           y * (box.height + box.spacing),
@@ -184,43 +185,45 @@ class Enemies {
       }
     }
 
-    // Group staring position
+    // group staring position
     this.ships.x = 100;
     this.ships.y = 50;
 
-    // Move the group edge to edge and loop
-    var tween = game.add.tween(this.ships).to(
+    // move the group edge to edge and loop
+    var tween: Phaser.Tween = game.add.tween(this.ships).to(
       { x: game.width - box.width * columns },
       6000 / (difficulty ? difficulty : 1),
       Phaser.Easing.Linear.None, true, 0, 1000, true
     );
 
-    // Descend on loop
+    // descend on loop
     tween.onRepeat.add(() => this.ships.y += game.height / 10, this);
 
   }
 
-  private setupEnemyShip(x: number, y: number, width: number, height: number, image: string) {
+  private setupEnemyShip(x: number, y: number, width: number, height: number, image: string): void  {
 
-    // Create the enemy ship
+    // create the enemy ship
     var ship: Phaser.Sprite = this.ships.create(x, y, image);
     ship.width = width;
     ship.height = height;
     ship.anchor.setTo(0.5, 0.5);
 
-    // Animate the ships
-    ship.animations.add('animate', null, 10, true);
-    ship.play('animate');
+    // animate the ships
+    ship.animations.add("animate", null, 10, true);
+    ship.play("animate");
     ship.body.moves = false;
 
   }
 
-  public update() {
+  public update(): void {
 
-    this.bullets.forEach((b : Phaser.Sprite) => b.angle++);
+    // this.bullets.forEach((b: Phaser.Sprite) => b.angle++);
 
-    // Reduce the fire cooldown
-    if (this.cooldown > 0) this.cooldown--;
+    // reduce the fire cooldown
+    if (this.cooldown > 0) {
+      this.cooldown--;
+    }
 
     if ( (game.rnd.integerInRange(1, 1000) / state.level) < 10 ) {
       this.fire();
@@ -228,30 +231,30 @@ class Enemies {
 
   }
 
-  public fire(speed: number = this.BULLET_SPEED) {
+  public fire(speed: number = this.BULLET_SPEED): void {
 
-    // Check cooldown
+    // check cooldown
     if (!this.cooldown) {
-      
-      // Get a bullet from the bullets pool
-      var bullet = this.bullets.getFirstExists(false);
-  
-      // If a bullet is available and at least one enemy alive
-      if (bullet && this.ships.getFirstAlive()) {
-   
-        // Randomly select one of them
-        var shooter: Phaser.Sprite = this.ships.getRandom();
 
-        // Bullet starting position below the shooter ship
+      // get a bullet from the bullets pool
+      let bullet: Phaser.Sprite = this.bullets.getFirstExists(false);
+
+      // if a bullet is available and at least one enemy alive
+      if (bullet && this.ships.getFirstAlive()) {
+
+        // randomly select one of them
+        let shooter: Phaser.Sprite = this.ships.getRandom();
+
+        // bullet starting position below the shooter ship
         bullet.reset(shooter.body.x, shooter.body.y);
 
-        // And fire the bullet from this enemy to the player
+        // fire the bullet from this enemy to the player
         game.physics.arcade.moveToObject(
           bullet, world.player.ship,
           this.BULLET_SPEED * state.level
         );
 
-        // Set up the cooldown
+        // set up the cooldown
         this.cooldown = this.FIRE_COOLDOWN;
 
       }
@@ -273,33 +276,31 @@ class State {
 
   }
 
-  private setupInput() {
+  private setupInput(): void {
 
-    // Set keyboard arrows as input
+    // set keyboard arrows as input
     this.cursors = game.input.keyboard.createCursorKeys();
 
-    // Set spacebar as fire key
+    // set spacebar as fire key
     this.fireKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
   }
 
-  public update() {
+  public update(): void {
 
-    // Update the player
+    // update the player
     world.player.update();
 
-    // Update the enemies
+    // update the enemies
     world.enemies.update();
 
-    // Move the player if right or left arrows are pressed
-    if (this.cursors.right.isDown || this.cursors.left.isDown) {
-      world.player.move(~~this.cursors.right.isDown - ~~this.cursors.left.isDown, 0);
-    }
+    // move the player if right or left arrows are pressed
+    world.player.move((this.cursors.right.isDown ? 1 : 0) - (this.cursors.left.isDown ? 1 : 0), 0);
 
-    // Fire if up arrow or the fire key are pressed
+    // fire if up arrow or the fire key are pressed
     if (this.cursors.up.isDown || this.fireKey.isDown) {
       world.player.fire();
-    };
+    }
 
   }
 
@@ -313,7 +314,7 @@ class World {
 
   constructor() {
 
-    // Initialize the game engine and elements
+    // initialize the game engine and elements
     game.scale.scaleMode = Phaser.ScaleManager.EXACT_FIT;
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -323,35 +324,35 @@ class World {
 
   }
 
-  private setuptBackground(image: string = 'background') {
+  private setuptBackground(image: string = "background"): void {
 
     this.background = game.add.tileSprite(0, 0, game.stage.width, game.stage.height, image);
 
   }
 
-  private setupPlayer(shipImage: string = 'player', bulletImage: string = 'playerBullet') {
+  private setupPlayer(shipImage: string = "player", bulletImage: string = "playerBullet"): void  {
 
     this.player = new Player(shipImage, bulletImage);
 
   }
 
-  private setupEnemies(shipImage: string = 'enemy', bulletImage: string = 'enemyBullet') {
+  private setupEnemies(shipImage: string = "enemy", bulletImage: string = "enemyBullet"): void  {
 
     this.enemies = new Enemies(shipImage, bulletImage);
 
   }
 
-  public update() {
+  public update(): void  {
 
-    // Scroll the background to simulate movement
+    // scroll the background to simulate movement
     this.scrollBackground();
 
   }
 
-  public scrollBackground(x: number = 0, y: number = 3) {
+  public scrollBackground(x: number = 0, y: number = 3): void  {
 
-    if (x) this.background.tilePosition.x += x;
-    if (y) this.background.tilePosition.y += y;
+    this.background.tilePosition.x += x;
+    this.background.tilePosition.y += y;
 
   }
 
