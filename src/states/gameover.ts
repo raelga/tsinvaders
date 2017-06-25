01c6c870acfca1f817ad579d30cf9ed75e4a8cd4
Rael Garcia <raelga@gmail.com>
2017-06-26 01:27:06 +0200

    Fix typo.

--- src/states/gameover.ts
+++ src/states/gameover.ts
@@ -14,7 +14,7 @@ export default class GameOverState extends Phaser.State {
 
     this.game.add.text(this.game.world.height / 3, this.game.world.height / 2 - 100, "Game Over", { font: "50px Arial", fill: "#fff" });
     this.game.add.text(this.game.world.height / 3, this.game.world.height / 2, this.message, { font: "50px Arial", fill: "#fff" });
-    this.game.add.text(this.game.world.height / 3, this.game.world.height / 2 + 100, "Finall Score: " + this.score, { font: "50px Arial", fill: "#fff" });
+    this.game.add.text(this.game.world.height / 3, this.game.world.height / 2 + 100, "Final Score: " + this.score, { font: "50px Arial", fill: "#fff" });
     this.game.add.text(this.game.world.height / 3, this.game.world.height - 80, "Press space to restart", { font: "50px Arial", fill: "#fff" });
 
     // look for spacebar press
